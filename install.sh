4232f24091e0f2f2fb5b4ecc306208cf7ed73e51
John Murray <me@johnmurray.io>
2013-08-31 00:41:41 -0400

    added make and sbt download to installer

--- install.sh
+++ install.sh
@@ -3,8 +3,9 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y git htop
+sudo apt-get install -y git htop make
 sudo apt-get install -y openjdk-7-jdk openjdk-7-jre
+wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.12.2/sbt.deb
 
 cd $HOME
 git clone git://github.com/JohnMurray/hello-akka.git hello-akka
@@ -12,3 +13,5 @@ cd ./hello-akka/
 
 wget http://downloads.typesafe.com/akka/akka-2.2.1.tgz
 tar xzf akka-2.2.1.tgz
+mv akka-2.2.1 akka
+rm akka-2.2.1.tgz
