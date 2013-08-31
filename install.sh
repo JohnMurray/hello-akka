3b88091dc7ac26b59b5f08e788e69a2b7315f831
John Murray <me@johnmurray.io>
2013-08-31 13:01:40 -0400

    Finished v0.1 installer for dev instances
    Just a simple script that setups up a blank dev-box for
    testing/working with Akka.

--- install.sh
+++ install.sh
@@ -3,11 +3,19 @@
 sudo apt-get update
 sudo apt-get upgrade
 
-sudo apt-get install -y git htop make
+sudo apt-get install -y git htop make dos2unix
 sudo apt-get install -y openjdk-7-jdk openjdk-7-jre
 wget http://scalasbt.artifactoryonline.com/scalasbt/sbt-native-packages/org/scala-sbt/sbt//0.12.2/sbt.deb
+sudo dpkg -i sbt.deb
 
 cd $HOME
+rm -rf dotfiles
+git clone git://github.com/JohnMurray/dotfiles.git
+cd dotfiles
+./install.py
+
+cd $HOME
+rm -rf hello-akka
 git clone git://github.com/JohnMurray/hello-akka.git hello-akka
 cd ./hello-akka/
 
@@ -15,3 +23,15 @@ wget http://downloads.typesafe.com/akka/akka-2.2.1.tgz
 tar xzf akka-2.2.1.tgz
 mv akka-2.2.1 akka
 rm akka-2.2.1.tgz
+dos2unix akka/bin/akka
+
+
+echo
+echo
+echo 'Getting ready to run...'
+echo
+echo
+
+sleep 2
+
+make run
